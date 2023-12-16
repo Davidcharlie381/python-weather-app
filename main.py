81f6258e8115a6f9f064f788b5714663b1e2d397
Charles <98989744+Davidcharlie381@users.noreply.github.com>
2023-12-16 21:37:35 +0100

    fix: make proper comments

--- main.py
+++ main.py
@@ -8,9 +8,20 @@ config.read("env.ini")
 
 api_key = config.get("variables", "api_key")
 
+# function to fetch weather 
+
 def get_weather(city):
+    """"
+    Fetch the weather of param {city}
+    """
+
+    # it could go wrong
+    
     try:
         res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}") 
+
+        # response status might not be OK
+        
         if res.status_code == 200:
             json_res = res.json()
             desc = json_res["weather"][0]["description"] 
@@ -21,15 +32,23 @@ def get_weather(city):
     except Exception as e:
         return str(e)
 
+# function to run when user clicks the button
+
 def make_request():
+    """
+    When the user clicks the button
+    """
+
     # Get text from the entry widget
+    
     entry_text = entry.get()
     
-    # Update the label with the entry's text
-    
+    # The result from the get_weather function passing in the entry_text as argument
     
     result = get_weather(entry_text)
-    
+
+    # Our function is expected to return a list of two things if all goes well.
+
     if isinstance(result, list):
         desc = result[0]
         temp = result[1]
@@ -37,17 +56,23 @@ def make_request():
         description.config(text=f"Desc: {desc}")
         temperature.config(text=f"Temp: {temp}")
         
+    # Else an error has been thrown
+    
     else:
         description.config(text=f"Error: {result}")
         temperature.config(text="")
 
+# Create a root window
+
 root = tk.Tk()
 root.title("Weather App")
 
-# Entry widget for user input
+# Label to instruct user
 
 instruction = tk.Label(root, text="Type in a city to get started:")
 instruction.pack(pady=20)
+
+# Entry to get user input
 
 entry = tk.Entry(root)
 entry.pack(pady=10)
@@ -56,11 +81,15 @@ entry.pack(pady=10)
 update_button = tk.Button(root, text="Check weather", command=make_request)
 update_button.pack()
 
+# Label widgets for description and temperature
+
 description = tk.Label(root, text="Desc: ")
 temperature = tk.Label(root, text="Temp: ")
 
 temperature.pack(pady=10)
 description.pack(pady=10)
+
+# Copyright because I wrote python, bro!!! 🥺
 
 built_by = tk.Label(root, text="Built by Charlie ©2023 ")
 built_by.pack(pady=50)
